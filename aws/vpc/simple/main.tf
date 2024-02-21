c7289ea01baf1b0e14a3205e6a82c39c2afb12e4
Paulo Rogerio Gomes E Silva <silva.paulo@ifood.com.br>
2024-02-21 16:04:42 -0300

    Definindo um range menor de sub-nets

--- aws/vpc/simple/main.tf
+++ aws/vpc/simple/main.tf
@@ -9,11 +9,13 @@ locals {
   ec2_public_subnet_cidrs = [
     cidrsubnet(var.cidr, 5, 0), # e.g. 10.0.0.0/21
     cidrsubnet(var.cidr, 5, 1), # e.g. 10.0.8.0/21
+    cidrsubnet(var.cidr, 5, 2), # e.g. 10.0.8.0/21
   ]
 
   ec2_private_subnet_cidrs = [
     cidrsubnet(var.cidr, 5, 5), 
     cidrsubnet(var.cidr, 5, 6),
+    cidrsubnet(var.cidr, 5, 7),
   ]
 
 
